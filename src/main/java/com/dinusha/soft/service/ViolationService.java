b2023da8c63b453880d8abe14942af0ddd103c48
jayas <24741358+djerror327@users.noreply.github.com>
2021-03-21 10:11:49 +0530

    get violation for all branches

--- src/main/java/com/dinusha/soft/service/ViolationService.java
+++ src/main/java/com/dinusha/soft/service/ViolationService.java
@@ -7,54 +7,57 @@ import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 public interface ViolationService {
     Logger LOGGER = Logger.getLogger(ViolationService.class);
 
-
     //        SonarQubeOpenViolationMonitor
-    Function<String, List<String>> GET_VIOLATION = url -> {
+    Function<String, Map<String, Integer>> GET_VIOLATION = url -> {
 
+        //given date
         String date = "2021-03";
+
+        Map<String, Integer> result = new HashMap<>();
 
         List<String> branches = BranchService.GET_BRANCHES.get();
 
-        String violation = Client.GET.apply("localhost:9000/api/issues/search?projectKeys=SonarQubeOpenViolationMonitor&resolved=false&branch=dev&ps=3");
-        JSONObject violationData = JsonUtil.JSON_OBJECT.apply(violation);
-
-        //calculate paging count
-        JSONObject paging = (JSONObject) violationData.get("paging");
-        long recursionCount = Paginate.RECURSION_COUNT.applyAsLong(paging);
-
-        //loop all pages and collect violation data
         //violation count for given YYYY-mm
         int violationCount = 0;
-        for (int page = 1; page <= recursionCount; page++) {
-            String violationObj = Client.GET.apply("localhost:9000/api/issues/search?projectKeys=SonarQubeOpenViolationMonitor&resolved=false&branch=dev&ps=3&p=" + page + "");
-            JSONObject jsonViolation = JsonUtil.JSON_OBJECT.apply(violationObj);
-            JSONArray issueArr = (JSONArray) jsonViolation.get("issues");
-            for (Object issue : issueArr) {
-                JSONObject issueObj = (JSONObject) issue;
-                String updateDate = issueObj.get("updateDate").toString();
-                String updateMonth = updateDate.substring(0, 7);
+        for (String branch : branches) {
 
-                //calculate violations for given month
-                if (updateMonth.equals(date)) {
-                    violationCount += 1;
+            //initialize violation count to 0 for each branch
+            violationCount = 0;
+            //paging related part
+            String pagingData = Client.GET.apply("localhost:9000/api/issues/search?projectKeys=SonarQubeOpenViolationMonitor&resolved=false&branch=" + branch + "&ps=3");
+            JSONObject pageObj = JsonUtil.JSON_OBJECT.apply(pagingData);
+
+            //calculate paging count
+            JSONObject paging = (JSONObject) pageObj.get("paging");
+            long recursionCount = Paginate.RECURSION_COUNT.applyAsLong(paging);
+
+            //loop all pages and collect violation data
+            for (int page = 1; page <= recursionCount; page++) {
+                String violationObj = Client.GET.apply("localhost:9000/api/issues/search?projectKeys=SonarQubeOpenViolationMonitor&resolved=false&branch=" + branch + "&ps=3&p=" + page + "");
+                JSONObject jsonViolation = JsonUtil.JSON_OBJECT.apply(violationObj);
+                JSONArray issueArr = (JSONArray) jsonViolation.get("issues");
+                for (Object issue : issueArr) {
+                    JSONObject issueObj = (JSONObject) issue;
+                    String updateDate = issueObj.get("updateDate").toString();
+                    String updateMonth = updateDate.substring(0, 7);
+
+                    //calculate violations for given month
+                    if (updateMonth.equals(date)) {
+                        violationCount += 1;
+                    }
                 }
-                System.out.println(updateMonth);
-
-
             }
-
-
-            System.out.println();
-
+            System.out.println("Violation count :" + violationCount);
+            result.put(branch, violationCount);
         }
-        System.out.println("Violation count :" + violationCount);
-        return null;
+        return result;
     };
-
 }
