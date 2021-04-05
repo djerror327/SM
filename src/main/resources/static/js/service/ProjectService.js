3125ddb1ece872d28dc7274e1abbc92f2a6703f9
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-05 08:54:56 +0530

    add search box

--- src/main/resources/static/js/service/ProjectService.js
+++ src/main/resources/static/js/service/ProjectService.js
@@ -1,6 +1,19 @@
+let objArr=[];
 async function loadProjectList() {
     const projectURL = projectAPI();
+
     await fetch(projectURL)
         .then(res => res.json())
-        .then(data => document.getElementById("a").innerHTML = JSON.stringify(data));
+        .then(data => {
+            const jsonArr = JSON.stringify(data)
+            objArr=JSON.parse(jsonArr);
+    
+            var select = document.getElementById('searchItem');
+            objArr.forEach((value, index) => {
+                var option = document.createElement("option");
+                option.innerHTML = value;
+                select.appendChild(option);
+
+            });
+        });
 }
