2a4e90be2f2a276d20129df14a9e93644f3e787e
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-08 09:21:14 +0530

    remove comments

--- src/main/resources/static/js/service/CbUService.js
+++ src/main/resources/static/js/service/CbUService.js
@@ -31,24 +31,18 @@ async function cbuService() {
             table.appendChild(tblRowHeaderSpace);
 
             for (users in userBranch) {
-                // const tblRowHeader = document.createElement('tr');
                 const tblRow = document.createElement('tr');
-                // const tblHeaderBranch = document.createElement('th');
                 const tblCellName = document.createElement('td');
                 const tblCellValue = document.createElement('td');
-                // const tblCellHeader = document.createTextNode(branch);
                 const tblAuthorhName = document.createTextNode(users);
                 const tblAuthorValue = document.createTextNode(userBranch[users]);
 
                 //set table data
-                // tblHeaderBranch.appendChild(tblCellHeader);
                 tblCellName.appendChild(tblAuthorhName);
                 tblCellValue.appendChild(tblAuthorValue);
 
-                // tblRowHeader.appendChild(tblHeaderBranch);
                 tblRow.appendChild(tblCellName);
                 tblRow.appendChild(tblCellValue);
-                // table.appendChild(tblRowHeader);
                 table.appendChild(tblRow);
             }
         }
