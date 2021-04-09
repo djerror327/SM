1cb8964c55e1f42bbab4446aada6b7a9b85cd602
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-09 20:09:06 +0530

    add validation

--- src/main/resources/static/js/controller/SearchController.js
+++ src/main/resources/static/js/controller/SearchController.js
@@ -14,16 +14,25 @@ function searchController() {
     let dropDown = document.getElementById('searchItem');
     let datePickerValue = document.getElementById('datePicker').value;
 
-    var dropDownValue = dropDown.options[dropDown.selectedIndex].text;
 
-    //set project name
-    document.getElementById('projectName').innerHTML = "Project : " + dropDownValue;
-    //set tile data
-    let val = violationTileService(dropDownValue, datePickerValue);
-    vbuService(dropDownValue, datePickerValue);
-    commitTileService(dropDownValue, datePickerValue);
-    cbuService(dropDownValue, datePickerValue);
+    //validation
+    const validate = btnSearchValidation(dropDown, datePickerValue);
 
-    //true == project scanned hide project not scanned text
-    projectNotScannedController(true);
+    if (validate) {
+
+        var dropDownValue = dropDown.options[dropDown.selectedIndex].text;
+        //set project name
+        document.getElementById('projectName').innerHTML = "Project : " + dropDownValue;
+        //set tile data
+        let val = violationTileService(dropDownValue, datePickerValue);
+        vbuService(dropDownValue, datePickerValue);
+        commitTileService(dropDownValue, datePickerValue);
+        cbuService(dropDownValue, datePickerValue);
+
+        //true == project scanned hide project not scanned text
+        projectNotScannedController(true);
+    }
+    else {
+        alert("Please check whether the project and date are selected !");
+    }
 }
