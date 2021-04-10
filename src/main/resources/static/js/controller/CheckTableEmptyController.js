04ea3e22baf003680d8404b1bf67e35cba98a37a
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-10 08:34:59 +0530

    add loading gif

--- src/main/resources/static/js/controller/CheckTableEmptyController.js
+++ src/main/resources/static/js/controller/CheckTableEmptyController.js
@@ -1,42 +1,35 @@
 function checkTableEmpty() {
-
     //violation tile data loading
     if ($('#tblVioation tr').length == 0) {
-
-        document.getElementById('violationTileLoading').innerHTML = "Data Loading...";
-        // console,console.log("table is empty",val);
+        document.getElementById('violationTileLoading').style.visibility = "visible";
     }
     else {
-        document.getElementById('violationTileLoading').innerHTML = "";
+        document.getElementById('violationTileLoading').style.visibility = "hidden";
     }
 
     //VbU tile data loading
     if ($('#tblVbU tr').length == 0) {
 
-        document.getElementById('vbuTileLoading').innerHTML = "Data Loading...";
-        // console,console.log("table is empty",val);
+        document.getElementById('vbuTileLoading').style.visibility = "visible";
     }
     else {
-        document.getElementById('vbuTileLoading').innerHTML = "";
+        document.getElementById('vbuTileLoading').style.visibility = "hidden";
     }
 
     //Commits tile data loading
     if ($('#tblCommits tr').length == 0) {
-
-        document.getElementById('CommitsTileLoading').innerHTML = "Data Loading...";
-        // console,console.log("table is empty",val);
+        document.getElementById('CommitsTileLoading').style.visibility = "visible";
     }
     else {
-        document.getElementById('CommitsTileLoading').innerHTML = "";
+        document.getElementById('CommitsTileLoading').style.visibility = "hidden";
     }
 
     //Commits tile data loading
     if ($('#tblCbU tr').length == 0) {
 
-        document.getElementById('cbuTileLoading').innerHTML = "Data Loading...";
-        // console,console.log("table is empty",val);
+        document.getElementById('cbuTileLoading').style.visibility = "visible";
     }
     else {
-        document.getElementById('cbuTileLoading').innerHTML = "";
+        document.getElementById('cbuTileLoading').style.visibility = "hidden";
     }
 }
