11593de1836e4e5c3eef2e61eda7a4af2968cb82
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-11 19:30:45 +0530

    change web client

--- src/main/java/com/dinusha/soft/utills/JsonUtil.java
+++ src/main/java/com/dinusha/soft/utills/JsonUtil.java
@@ -11,7 +11,7 @@ import java.util.function.Function;
 
 @Component
 public class JsonUtil {
-    Logger logger = Logger.getLogger(JsonUtil.class);
+    private static final Logger logger = Logger.getLogger(JsonUtil.class);
     public final Function<String, JSONObject> jsonObject = value -> {
         logger.debug("Passing to JSON Object");
         JSONParser parser = new JSONParser();
@@ -27,12 +27,12 @@ public class JsonUtil {
     public final Function<String, JSONArray> jsonArray = value -> {
         logger.debug("Passing to JSON Array");
         JSONParser parser = new JSONParser();
-        JSONArray jsonArray = null;
+        JSONArray jsonArr = null;
         try {
-            jsonArray = (JSONArray) parser.parse(value);
+            jsonArr = (JSONArray) parser.parse(value);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        return jsonArray;
+        return jsonArr;
     };
 }
--- src/main/java/com/dinusha/soft/webclient/Client.java
+++ src/main/java/com/dinusha/soft/webclient/Client.java
@@ -1,39 +1,60 @@
 package com.dinusha.soft.webclient;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.util.retry.Retry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
-import java.time.Duration;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.function.BinaryOperator;
-import java.util.function.UnaryOperator;
 
-@Component
+@Configuration
 public class Client {
-    Logger logger = Logger.getLogger(Client.class);
-    public final UnaryOperator<String> get = url -> {
-        WebClient webClient = WebClient.create();
-        logger.info("GET -> " + url);
-        return Objects.requireNonNull(webClient.get()
-                .uri(url)
-                .exchange()
-                .block())
-                .bodyToMono(String.class).retryWhen(Retry.fixedDelay(10, Duration.ofMillis(5000)))
-                .block();
+    private static final Logger logger = Logger.getLogger(Client.class);
+    public final BinaryOperator<String> getWithAuthHeader = (authHeader, uri) -> {
+        logger.info("GET -> " + uri);
+        String result = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(uri);
+        request.setHeader("Authorization", authHeader);
+        CloseableHttpResponse response = null;
+        try {
+            response = client.execute(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HttpEntity entity = Objects.requireNonNull(response).getEntity();
+        try {
+            result = EntityUtils.toString(entity);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     };
+    @Value("${web.client.retry.count}")
+    private String retryCount;
 
-    public final BinaryOperator<String> getWithAuthHeader = (authHeader, url) -> {
-        WebClient webClient = WebClient.create();
-        logger.info("GET -> " + url);
-        return Objects.requireNonNull(webClient.get()
-                .uri(url)
-                .header("Authorization", authHeader)
-                .exchange()
-                .block())
-                .bodyToMono(String.class).retryWhen(Retry.fixedDelay(10, Duration.ofMillis(5000)))
-                .block();
-    };
 
+    //    public final UnaryOperator<String> get = url -> {
+////        WebClient webClient = WebClient.create();
+//        logger.info("GET -> " + url);
+//        return Objects.requireNonNull(
+//                webClientBuilder()
+//                        .build()
+//                        .get()
+//                        .uri(url)
+//                        .exchange().block())
+//                .bodyToMono(String.class)
+//                .retryWhen(Retry.fixedDelay(Integer.parseInt(retryCount), Duration.ofMillis(Integer.parseInt(retryDelay))))
+//                .block()
+//                ;
+//    };
+    @Value("${web.client.retry.delay}")
+    private String retryDelay;
 }
