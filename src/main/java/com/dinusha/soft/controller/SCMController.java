cf866263b3af99d9a01bc8291beaed0099139b49
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-21 12:35:26 +0530

    cache enable option

--- src/main/java/com/dinusha/soft/controller/SCMController.java
+++ src/main/java/com/dinusha/soft/controller/SCMController.java
@@ -1,7 +1,6 @@
 package com.dinusha.soft.controller;
 
 import com.dinusha.soft.cache.SonarCache;
-import com.dinusha.soft.service.SCMService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,20 +13,12 @@ import java.util.Map;
 public class SCMController {
     private static final Logger logger = Logger.getLogger(SCMController.class);
     @Autowired
-    private SCMService scmService;
-    @Autowired
     private SonarCache sonarCache;
 
     @GetMapping("/v1/scm/commits/{projectKey}/{date}")
     public String getCommits(@PathVariable String projectKey, @PathVariable String date) {
         logger.debug("GET : /v1/scm/commits/" + projectKey + "/" + date);
-//        sonarCache.createSCMCache(projectKey, date);
-//        sonarCache.checkSCMCache(projectKey, date);
-//        return scmService.getCommits.apply(projectKey, date);
-        Map<String, String> cacheData = sonarCache.checkAnalysisCache.apply(projectKey, date);
-//        List<Object> response = new ArrayList<>();
-//        response.add(cacheData.get("scm"));
+        Map<String, String> cacheData = sonarCache.checkCacheEnableSCM.apply(projectKey, date);
         return cacheData.get("scm");
-//        return null;
     }
 }
--- src/main/java/com/dinusha/soft/controller/ViolationController.java
+++ src/main/java/com/dinusha/soft/controller/ViolationController.java
@@ -1,7 +1,6 @@
 package com.dinusha.soft.controller;
 
 import com.dinusha.soft.cache.SonarCache;
-import com.dinusha.soft.service.ViolationService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,23 +13,12 @@ import java.util.Map;
 public class ViolationController {
     private static final Logger logger = Logger.getLogger(ViolationController.class);
     @Autowired
-    private ViolationService violationService;
-    @Autowired
     private SonarCache sonarCache;
 
     @GetMapping("/v1/violations/{projectKey}/{date}")
     public String getViolations(@PathVariable String projectKey, @PathVariable String date) {
         logger.debug("GET : /v1/violations/" + projectKey + "/" + date);
-//        sonarCache.createViolationCache(projectKey, date);
-        Map<String, String> cacheData = sonarCache.checkAnalysisCache.apply(projectKey, date);
-//        List<Object> response = new ArrayList<>();
-//        response.add(cacheData.get("violation"));
-//        System.out.println("controller get violation : " + cacheData.get("violation"));
-//        System.out.println("debug controller response : " + response);
-//        System.out.println("debug controller realtime : " + violationService.getViolation.apply(projectKey, date));
+        Map<String, String> cacheData = sonarCache.checkCacheEnableViolation.apply(projectKey, date);
         return cacheData.get("violation");
-
-//        return violationService.getViolation.apply(projectKey, date);
-//        return null;
     }
 }
