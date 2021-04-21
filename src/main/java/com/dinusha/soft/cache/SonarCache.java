d94c5d01b502390b8498e7345ac1db43ebaffc4b
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-21 09:15:34 +0530

    add loggers

--- src/main/java/com/dinusha/soft/cache/SonarCache.java
+++ src/main/java/com/dinusha/soft/cache/SonarCache.java
@@ -56,7 +56,8 @@ public class SonarCache {
         String jsonViolationArray = jsonUtil.listToJsonStringArray.apply(violationList);
 
         //save it into json file in the cache folder
-        System.out.println(jsonViolationArray);
+        logger.info("Creating violation cache : project key : " + projectKey + " : date : " + date);
+        logger.debug("Writing into violation cache : " + jsonViolationArray);
         createCacheFile(jsonViolationArray, projectKey, date, "violation");
         return jsonViolationArray;
     };
@@ -67,6 +68,8 @@ public class SonarCache {
         String jsonSCMnArray = jsonUtil.listToJsonStringArray.apply(scmList);
 
         //save it into json file in the cache folder
+        logger.info("Creating SCM cache : project key : " + projectKey + " : date : " + date);
+        logger.debug("Writing into SCM cache : " + jsonSCMnArray);
         createCacheFile(jsonSCMnArray, projectKey, date, "scm");
         return jsonSCMnArray;
     };
@@ -77,32 +80,36 @@ public class SonarCache {
             Path path = Paths.get(folderPAth);
             Files.createDirectories(path);
             Path filePath = Paths.get(folderPAth + "/" + "branchAnalysis.json");
+            logger.info("Creating branchAnalysis.json : project key : " + projectKey);
             Files.write(filePath, json.getBytes(StandardCharsets.UTF_8));
-            System.out.println("File created");
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e.getStackTrace());
         }
     };
 
     private final Consumer<String> createBranchAnalysisCacheJson = projectKey -> {
         Map<String, Map<String, String>> branchesAnalysis = analysisService.getBranchesAnalysis(projectKey);
         JSONObject jsonObject = jsonUtil.mapToJsonObject.apply(branchesAnalysis);
+        logger.debug("Creating Branch Analysis cache data : project key : " + projectKey);
         createCacheAnalysisFile.accept(jsonObject.toJSONString(), projectKey);
-        System.out.println(jsonObject);
     };
 
-    private final BooleanSupplier checkCacheFolderExist = () -> Files.isDirectory(Paths.get(CACHE_PATH));
+    private final BooleanSupplier checkCacheFolderExist = () -> {
+        logger.debug("Checking cache folder exist");
+        return Files.isDirectory(Paths.get(CACHE_PATH));
+    };
 
     private final BiPredicate<String, String> checkViolationCache = (String projectKey, String date) -> {
         String folderPAth = CACHE_PATH + projectKey + "/" + date;
         Path violationPath = Paths.get(folderPAth + "/violation.json");
+        logger.debug("Checking violation cache json exist : projectKey: " + projectKey + " +date : " + date + " path : " + violationPath);
         return Files.exists(violationPath);
     };
 
     private final BiPredicate<String, String> checkSCMCache = (String projectKey, String date) -> {
         String folderPAth = CACHE_PATH + projectKey + "/" + date;
         Path scmPath = Paths.get(folderPAth + "/scm.json");
-        System.out.println("scm path " + Files.exists(scmPath));
+        logger.debug("Checking SCM cache json exist : projectKey: " + projectKey + " +date : " + date + " path : " + scmPath);
         return Files.exists(scmPath);
     };
     private final Function<String, StringBuilder> readCacheFile = folderPAth -> {
@@ -110,11 +117,11 @@ public class SonarCache {
         try {
             data = (Files.readAllLines(Paths.get(folderPAth)));
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e.getStackTrace());
         }
         StringBuilder analysisFileContent = new StringBuilder();
         Objects.requireNonNull(data).forEach(analysisFileContent::append);
-        System.out.println("JSON FILE CONTENT FOR STRING BUILDER :" + analysisFileContent);
+        logger.info("Reading cache file : " + folderPAth);
         return analysisFileContent;
     };
 
@@ -129,6 +136,8 @@ public class SonarCache {
         HashMap<String, Long> timestamp = new HashMap<>();
         timestamp.put("dateMillis", cachedTimestamp.getMillis());
         timestamp.put("apiMillis", apiTimestamp.getMillis());
+        logger.debug("Convert date into milliseconds : " + date + " -> " + cachedTimestamp.getMillis());
+        logger.debug("Convert API date  into milliseconds : " + apiDate + " -> " + apiTimestamp.getMillis());
         return timestamp;
     };
 
@@ -136,6 +145,7 @@ public class SonarCache {
         Map<String, String> cachedFileData = new HashMap<>();
         cachedFileData.put("violation", violation);
         cachedFileData.put("scm", scm);
+        logger.debug("Returning  violations and SCM  String data as Map");
         return cachedFileData;
     };
 
@@ -155,7 +165,6 @@ public class SonarCache {
         String folderPAth = CACHE_PATH + projectKey + "/branchAnalysis.json";
         String violationPath = CACHE_PATH + projectKey + "/" + date + "/" + "violation.json";
         String scmPath = CACHE_PATH + projectKey + "/" + date + "/" + "scm.json";
-
         if (checkCacheFolderExist.getAsBoolean()) {
             StringBuilder analysisFileContent = readCacheFile.apply(folderPAth);
             JSONObject cachedJsonProject = jsonUtil.stringToJsonObject.apply(analysisFileContent.toString());
@@ -164,10 +173,12 @@ public class SonarCache {
                 for (Object apiKey : jsonBranchesAPI.keySet()) {
                     //get api date
                     String apiDate = (jsonBranchesAPI.get(apiKey).toString()).substring(0, 10);
+                    logger.debug("Removing API last analysis time and read only date");
                     //convert to 1st of the current date od api for ui match
                     Map<String, Long> timestamp = getMillis.apply(date, apiDate);
                     //check cache folder is exist. if not create cache
                     //if date is smaller than api then return cache if not exist then create cache
+                    logger.debug("Checking date milliseconds are less than API milliseconds");
                     if (timestamp.get("dateMillis") < timestamp.get("apiMillis")) {
                         //check cache available the return else create cache and return
                         if (checkViolationCache.test(projectKey, date) && checkSCMCache.test(projectKey, date)) {
@@ -180,9 +191,11 @@ public class SonarCache {
                             return getCachedData.apply(violation, scm);
                         }
                     } else if (timestamp.get("dateMillis").equals(timestamp.get("apiMillis"))) {
+                        logger.debug("Checking date milliseconds and API milliseconds are equal");
                         //if reading for current month
                         JSONObject cachedJsonBranches = (JSONObject) cachedJsonProject.get(key);
                         if (cachedJsonBranches.size() != branchesAPISize) {
+                            logger.debug("Checking cached json branch and API branches sizes are different");
                             //create cache
                             //create branch analysis cache json
                             createBranchAnalysisCacheJson.accept(projectKey);
@@ -197,6 +210,7 @@ public class SonarCache {
                                 String apiDateCurrentMonth = (jsonBranchesAPI.get(cachedKey).toString()).substring(0, 10);
                                 Map<String, Long> millis = getMillis.apply(cachedDate, apiDateCurrentMonth);
                                 if (millis.get("dateMillis") < (millis.get("apiMillis"))) {
+                                    logger.debug("Checking date milliseconds are less than API milliseconds for current month");
                                     //create branch analysis cache json
                                     createBranchAnalysisCacheJson.accept(projectKey);
                                     String violation = createViolationCache.apply(projectKey, date);
@@ -207,11 +221,13 @@ public class SonarCache {
 
                             }
                             if (checkViolationCache.test(projectKey, date) && checkSCMCache.test(projectKey, date)) {
+                                logger.debug("Checking cache data is available for current month. return cached data");
                                 return getCachedData.apply(String.valueOf(readCacheFile.apply(violationPath)), String.valueOf(readCacheFile.apply(scmPath)));
                             }
                             //if cache file not available for current month
                             else {
                                 //create branch analysis cache json
+                                logger.debug("No cache data is available for current month. creating cache for current month");
                                 createBranchAnalysisCacheJson.accept(projectKey);
                                 String violation = createViolationCache.apply(projectKey, date);
                                 String scm = createSCMCache.apply(projectKey, date);
@@ -222,6 +238,7 @@ public class SonarCache {
                     }
                     //ui timestamp is greater than api timestamp
                     else {
+                        logger.debug("Scanning for future date. Directly reading from API");
                         List<Object> violationAPI = violationService.getViolation.apply(projectKey, date);
                         List<Object> scmAPI = scmService.getCommits.apply(projectKey, date);
                         JSONArray jsonArrViolation = jsonUtil.listToJsonArray.apply(violationAPI);
@@ -232,6 +249,7 @@ public class SonarCache {
                 }
             }
         } else {
+            logger.debug("Cache folder is not available.");
             //create branch analysis cache json
             createBranchAnalysisCacheJson.accept(projectKey);
             //if cache folder is not available re create cache
@@ -248,10 +266,10 @@ public class SonarCache {
             Path path = Paths.get(folderPAth);
             Files.createDirectories(path);
             Path filePath = Paths.get(folderPAth + "/" + fileName + ".json");
+            logger.debug("Creating  cache file : " + folderPAth + "/" + fileName + ".json");
             Files.write(filePath, json.getBytes(StandardCharsets.UTF_8));
-            System.out.println("File created");
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e.getStackTrace());
         }
     }
 }
