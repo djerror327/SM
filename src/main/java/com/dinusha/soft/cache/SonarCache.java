e5126b4665538710f00e2b51dc6aa3e104e02c61
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-21 14:45:39 +0530

    change order of methods

--- src/main/java/com/dinusha/soft/cache/SonarCache.java
+++ src/main/java/com/dinusha/soft/cache/SonarCache.java
@@ -38,117 +38,8 @@ public class SonarCache {
     private JsonUtil jsonUtil;
     @Autowired
     private AnalysisService analysisService;
-    @SuppressWarnings("rawtypes")
-    private final BiFunction<String, String, Map<String, String>> checkAnalysisCache = (projectKey, date) -> {
-        //api analysis
-        Map<String, Map<String, String>> branchesAnalysis = analysisService.getBranchesAnalysis(projectKey);
-        JSONObject jsonObject = jsonUtil.mapToJsonObject.apply(branchesAnalysis);
-        JSONObject jsonBranchesAPI = new JSONObject();
-
-        int branchesAPISize = 0;
-        for (Object key : jsonObject.keySet()) {
-            jsonBranchesAPI = new JSONObject((Map) jsonObject.get(key));
-            branchesAPISize = jsonBranchesAPI.size();
-        }
-        //cache analysis
-        String folderPAth = CACHE_PATH + projectKey + "/branchAnalysis.json";
-        String violationPath = CACHE_PATH + projectKey + "/" + date + "/" + "violation.json";
-        String scmPath = CACHE_PATH + projectKey + "/" + date + "/" + "scm.json";
-        if (checkCacheFolderExist.getAsBoolean()) {
-            StringBuilder analysisFileContent = readCacheFile.apply(folderPAth);
-            JSONObject cachedJsonProject = jsonUtil.stringToJsonObject.apply(analysisFileContent.toString());
-            for (Object key : cachedJsonProject.keySet()) {
-                //if month is less than the api latest analysis month the return the cache if available. else get data from api and create a cached and return the cache file
-                for (Object apiKey : jsonBranchesAPI.keySet()) {
-                    //get api date
-                    String apiDate = (jsonBranchesAPI.get(apiKey).toString()).substring(0, 10);
-                    logger.debug("Removing API last analysis time and read only date");
-                    //convert to 1st of the current date od api for ui match
-                    Map<String, Long> timestamp = getMillis.apply(date, apiDate);
-                    //check cache folder is exist. if not create cache
-                    //if date is smaller than api then return cache if not exist then create cache
-                    logger.debug("Checking date milliseconds are less than API milliseconds");
-                    if (timestamp.get("dateMillis") < timestamp.get("apiMillis")) {
-                        //check cache available the return else create cache and return
-                        if (checkViolationCache.test(projectKey, date) && checkSCMCache.test(projectKey, date)) {
-                            return getCachedData.apply(String.valueOf(readCacheFile.apply(violationPath)), String.valueOf(readCacheFile.apply(scmPath)));
-                        } else {
-                            //create branch analysis cache json
-                            createBranchAnalysisCacheJson.accept(projectKey);
-                            String violation = createViolationCache.apply(projectKey, date);
-                            String scm = createSCMCache.apply(projectKey, date);
-                            return getCachedData.apply(violation, scm);
-                        }
-                    } else if (timestamp.get("dateMillis").equals(timestamp.get("apiMillis"))) {
-                        logger.debug("Checking date milliseconds and API milliseconds are equal");
-                        //if reading for current month
-                        JSONObject cachedJsonBranches = (JSONObject) cachedJsonProject.get(key);
-                        if (cachedJsonBranches.size() != branchesAPISize) {
-                            logger.debug("Checking cached json branch and API branches sizes are different");
-                            //create cache
-                            //create branch analysis cache json
-                            createBranchAnalysisCacheJson.accept(projectKey);
-                            String violation = createViolationCache.apply(projectKey, date);
-                            String scm = createSCMCache.apply(projectKey, date);
-                            //create branch analysis cache json
-                            return getCachedData.apply(violation, scm);
-                        } else {
-                            //check api branch analysis and cached jason file timestamp. if there is time mismatch then recreate cache
-                            for (Object cachedKey : cachedJsonBranches.keySet()) {
-                                String cachedDate = (cachedJsonBranches.get(cachedKey).toString()).substring(0, 10);
-                                String apiDateCurrentMonth = (jsonBranchesAPI.get(cachedKey).toString()).substring(0, 10);
-                                Map<String, Long> millis = getMillis.apply(cachedDate, apiDateCurrentMonth);
-                                if (millis.get("dateMillis") < (millis.get("apiMillis"))) {
-                                    logger.debug("Checking date milliseconds are less than API milliseconds for current month");
-                                    //create branch analysis cache json
-                                    createBranchAnalysisCacheJson.accept(projectKey);
-                                    String violation = createViolationCache.apply(projectKey, date);
-                                    String scm = createSCMCache.apply(projectKey, date);
-                                    //create branch analysis cache json
-                                    return getCachedData.apply(violation, scm);
-                                }
-
-                            }
-                            if (checkViolationCache.test(projectKey, date) && checkSCMCache.test(projectKey, date)) {
-                                logger.debug("Checking cache data is available for current month. return cached data");
-                                return getCachedData.apply(String.valueOf(readCacheFile.apply(violationPath)), String.valueOf(readCacheFile.apply(scmPath)));
-                            }
-                            //if cache file not available for current month
-                            else {
-                                //create branch analysis cache json
-                                logger.debug("No cache data is available for current month. creating cache for current month");
-                                createBranchAnalysisCacheJson.accept(projectKey);
-                                String violation = createViolationCache.apply(projectKey, date);
-                                String scm = createSCMCache.apply(projectKey, date);
-                                //create branch analysis cache json
-                                return getCachedData.apply(violation, scm);
-                            }
-                        }
-                    }
-                    //ui timestamp is greater than api timestamp
-                    else {
-                        logger.debug("Scanning for future date. Directly reading from API");
-                        List<Object> violationAPI = violationService.getViolation.apply(projectKey, date);
-                        List<Object> scmAPI = scmService.getCommits.apply(projectKey, date);
-                        JSONArray jsonArrViolation = jsonUtil.listToJsonArray.apply(violationAPI);
-                        JSONArray jsonArrSCM = jsonUtil.listToJsonArray.apply(scmAPI);
-                        //create cache file
-                        return getCachedData.apply(jsonArrViolation.toJSONString(), jsonArrSCM.toJSONString());
-                    }
-                }
-            }
-        } else {
-            logger.debug("Cache folder is not available.");
-            //create branch analysis cache json
-            createBranchAnalysisCacheJson.accept(projectKey);
-            //if cache folder is not available re create cache
-            String violation = createViolationCache.apply(projectKey, date);
-            String scm = createSCMCache.apply(projectKey, date);
-            return getCachedData.apply(violation, scm);
-        }
-        return null;
-    };
-
+    @Value("${cache.enable}")
+    private String cacheEnable;
 
     public final BooleanSupplier deleteAllCache = () -> {
         try {
@@ -259,8 +150,117 @@ public class SonarCache {
         logger.debug("Returning  violations and SCM  String data as Map");
         return cachedFileData;
     };
-    @Value("${cache.enable}")
-    private String cacheEnable;
+
+    @SuppressWarnings("rawtypes")
+    private final BiFunction<String, String, Map<String, String>> checkAnalysisCache = (projectKey, date) -> {
+        //api analysis
+        Map<String, Map<String, String>> branchesAnalysis = analysisService.getBranchesAnalysis(projectKey);
+        JSONObject jsonObject = jsonUtil.mapToJsonObject.apply(branchesAnalysis);
+        JSONObject jsonBranchesAPI = new JSONObject();
+
+        int branchesAPISize = 0;
+        for (Object key : jsonObject.keySet()) {
+            jsonBranchesAPI = new JSONObject((Map) jsonObject.get(key));
+            branchesAPISize = jsonBranchesAPI.size();
+        }
+        //cache analysis
+        String folderPAth = CACHE_PATH + projectKey + "/branchAnalysis.json";
+        String violationPath = CACHE_PATH + projectKey + "/" + date + "/" + "violation.json";
+        String scmPath = CACHE_PATH + projectKey + "/" + date + "/" + "scm.json";
+        if (checkCacheFolderExist.getAsBoolean()) {
+            StringBuilder analysisFileContent = readCacheFile.apply(folderPAth);
+            JSONObject cachedJsonProject = jsonUtil.stringToJsonObject.apply(analysisFileContent.toString());
+            for (Object key : cachedJsonProject.keySet()) {
+                //if month is less than the api latest analysis month the return the cache if available. else get data from api and create a cached and return the cache file
+                for (Object apiKey : jsonBranchesAPI.keySet()) {
+                    //get api date
+                    String apiDate = (jsonBranchesAPI.get(apiKey).toString()).substring(0, 10);
+                    logger.debug("Removing API last analysis time and read only date");
+                    //convert to 1st of the current date od api for ui match
+                    Map<String, Long> timestamp = getMillis.apply(date, apiDate);
+                    //check cache folder is exist. if not create cache
+                    //if date is smaller than api then return cache if not exist then create cache
+                    logger.debug("Checking date milliseconds are less than API milliseconds");
+                    if (timestamp.get("dateMillis") < timestamp.get("apiMillis")) {
+                        //check cache available the return else create cache and return
+                        if (checkViolationCache.test(projectKey, date) && checkSCMCache.test(projectKey, date)) {
+                            return getCachedData.apply(String.valueOf(readCacheFile.apply(violationPath)), String.valueOf(readCacheFile.apply(scmPath)));
+                        } else {
+                            //create branch analysis cache json
+                            createBranchAnalysisCacheJson.accept(projectKey);
+                            String violation = createViolationCache.apply(projectKey, date);
+                            String scm = createSCMCache.apply(projectKey, date);
+                            return getCachedData.apply(violation, scm);
+                        }
+                    } else if (timestamp.get("dateMillis").equals(timestamp.get("apiMillis"))) {
+                        logger.debug("Checking date milliseconds and API milliseconds are equal");
+                        //if reading for current month
+                        JSONObject cachedJsonBranches = (JSONObject) cachedJsonProject.get(key);
+                        if (cachedJsonBranches.size() != branchesAPISize) {
+                            logger.debug("Checking cached json branch and API branches sizes are different");
+                            //create cache
+                            //create branch analysis cache json
+                            createBranchAnalysisCacheJson.accept(projectKey);
+                            String violation = createViolationCache.apply(projectKey, date);
+                            String scm = createSCMCache.apply(projectKey, date);
+                            //create branch analysis cache json
+                            return getCachedData.apply(violation, scm);
+                        } else {
+                            //check api branch analysis and cached jason file timestamp. if there is time mismatch then recreate cache
+                            for (Object cachedKey : cachedJsonBranches.keySet()) {
+                                String cachedDate = (cachedJsonBranches.get(cachedKey).toString()).substring(0, 10);
+                                String apiDateCurrentMonth = (jsonBranchesAPI.get(cachedKey).toString()).substring(0, 10);
+                                Map<String, Long> millis = getMillis.apply(cachedDate, apiDateCurrentMonth);
+                                if (millis.get("dateMillis") < (millis.get("apiMillis"))) {
+                                    logger.debug("Checking date milliseconds are less than API milliseconds for current month");
+                                    //create branch analysis cache json
+                                    createBranchAnalysisCacheJson.accept(projectKey);
+                                    String violation = createViolationCache.apply(projectKey, date);
+                                    String scm = createSCMCache.apply(projectKey, date);
+                                    //create branch analysis cache json
+                                    return getCachedData.apply(violation, scm);
+                                }
+
+                            }
+                            if (checkViolationCache.test(projectKey, date) && checkSCMCache.test(projectKey, date)) {
+                                logger.debug("Checking cache data is available for current month. return cached data");
+                                return getCachedData.apply(String.valueOf(readCacheFile.apply(violationPath)), String.valueOf(readCacheFile.apply(scmPath)));
+                            }
+                            //if cache file not available for current month
+                            else {
+                                //create branch analysis cache json
+                                logger.debug("No cache data is available for current month. creating cache for current month");
+                                createBranchAnalysisCacheJson.accept(projectKey);
+                                String violation = createViolationCache.apply(projectKey, date);
+                                String scm = createSCMCache.apply(projectKey, date);
+                                //create branch analysis cache json
+                                return getCachedData.apply(violation, scm);
+                            }
+                        }
+                    }
+                    //ui timestamp is greater than api timestamp
+                    else {
+                        logger.debug("Scanning for future date. Directly reading from API");
+                        List<Object> violationAPI = violationService.getViolation.apply(projectKey, date);
+                        List<Object> scmAPI = scmService.getCommits.apply(projectKey, date);
+                        JSONArray jsonArrViolation = jsonUtil.listToJsonArray.apply(violationAPI);
+                        JSONArray jsonArrSCM = jsonUtil.listToJsonArray.apply(scmAPI);
+                        //create cache file
+                        return getCachedData.apply(jsonArrViolation.toJSONString(), jsonArrSCM.toJSONString());
+                    }
+                }
+            }
+        } else {
+            logger.debug("Cache folder is not available.");
+            //create branch analysis cache json
+            createBranchAnalysisCacheJson.accept(projectKey);
+            //if cache folder is not available re create cache
+            String violation = createViolationCache.apply(projectKey, date);
+            String scm = createSCMCache.apply(projectKey, date);
+            return getCachedData.apply(violation, scm);
+        }
+        return null;
+    };
 
     private void createCacheFile(String json, String projectKey, String date, String fileName) {
         String folderPAth = CACHE_PATH + projectKey + "/" + date;
