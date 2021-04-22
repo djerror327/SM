fd926d0dbf3ea4ad53fbd29d003a4f07efdbf942
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-22 10:10:42 +0530

    add scm cache

--- src/main/java/com/dinusha/soft/controller/SCMController.java
+++ src/main/java/com/dinusha/soft/controller/SCMController.java
@@ -1,6 +1,6 @@
 package com.dinusha.soft.controller;
 
-import com.dinusha.soft.cache.SonarCache;
+import com.dinusha.soft.cache.SCMCache;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +13,12 @@ import java.util.Map;
 public class SCMController {
     private static final Logger logger = Logger.getLogger(SCMController.class);
     @Autowired
-    private SonarCache sonarCache;
+    private SCMCache scmCache;
 
     @GetMapping("/v1/scm/commits/{projectKey}/{date}")
     public String getCommits(@PathVariable String projectKey, @PathVariable String date) {
         logger.debug("GET : /v1/scm/commits/" + projectKey + "/" + date);
-        Map<String, String> cacheData = sonarCache.checkCacheEnableSCM.apply(projectKey, date);
+        Map<String, String> cacheData = scmCache.checkCacheEnableSCM.apply(projectKey, date);
         return cacheData.get("scm");
     }
 }
